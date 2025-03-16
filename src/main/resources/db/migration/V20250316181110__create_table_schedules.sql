4b33f5b65bedbf374019ae759da67d967efd74a7
Diego de Mello <diegodemello027@gmail.com>
2025-03-16 19:33:09 -0300

    Configurando o spring e mysql.

---
+++ src/main/resources/db/migration/V20250316181110__create_table_schedules.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS SCHEDULES (
+    id BIGINT not null primary key,
+    start_at timestamp not null,
+    end_at timestamp not null,
+    client_id BIGINT not null,
+    CONSTRAINT UK_SCHEDULE_INTERVAL  UNIQUE (start_at, end_at),
+    CONSTRAINT FK_CLIENTS_SCHEDULES FOREIGN KEY(client_id) REFERENCES CLIENTS(id)
+);
